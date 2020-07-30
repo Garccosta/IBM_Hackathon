bc162d7b2cea97f6727cf7eb813ca550883994e3
garccosta <garccosta@gmail.com>
2020-07-30 20:35:57 -0300

    Updates

---
+++ precache-manifest.2eae7fba07a3bb0c5a392005561e6357.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7edfaa741838dd9a18ddf1b7ed495d0e",
+    "url": "/IBM_Hackathon/index.html"
+  },
+  {
+    "revision": "610156bff851fa28dab7",
+    "url": "/IBM_Hackathon/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "685aa41c05e4fea0ba7d",
+    "url": "/IBM_Hackathon/static/css/main.5fc48634.chunk.css"
+  },
+  {
+    "revision": "610156bff851fa28dab7",
+    "url": "/IBM_Hackathon/static/js/2.175635e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IBM_Hackathon/static/js/2.175635e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "685aa41c05e4fea0ba7d",
+    "url": "/IBM_Hackathon/static/js/main.8dd20664.chunk.js"
+  },
+  {
+    "revision": "709ae9db54ac46e7d65a",
+    "url": "/IBM_Hackathon/static/js/runtime-main.88fb7102.js"
+  }
+]);
